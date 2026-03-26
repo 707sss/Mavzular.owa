e248726d3ea02b8db20c0232c7ae204a6503ada3
707sss <github707sss@gmail.com>
2026-03-26 18:01:28 +0500

    Implement loops to print numbers and greetings
    Add two for loops to display numbers and greetings

--- for.php
+++ for.php
@@ -1,1 +1,18 @@
+<?php
+for ($i = 1; $i <= 5; $i++){
+    echo $i . "<br>";
+}                                                       //raqamni ko'paytirish
 
+
+
+for ($i = 1; $i <= 5; $i++){
+    echo "salom";
+}                                                        //>salom< textni ko'paytirish  ~/n~ <br> vazifasi
+
+$fruits = ["olma","anor","banan"];
+for
+
+
+
+
+?>
